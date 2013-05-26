c75ce26dba3dfc4da5043a9aa7ecb70c943b6283
Soedar <soedar.sg@gmail.com>
2013-05-26 11:07:33 -0700

    Added templates, and added routes for all the pages

--- turtlegram.py
+++ turtlegram.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run()
